d46655733944e270174d774f9f1c848d95443ff0
Nikolay Naumenko <nnv2006@ukr.net>
2023-03-18 00:23:08 +0200

    18-03-2023

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  ImageBackground,
+  View,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Text,
+} from "react-native";
+import RegistrationScreen from "./src/Screens/RegistrationScreen/RegistrationScreen";
+import * as SplashScreen from "expo-splash-screen";
+import { useState, useEffect } from "react";
+import { useFonts } from "expo-font";
+import LoginScreen from "./src/Screens/LoginScreen/LoginScreen";
+
+const backgroundImage = require("./assets/images/bg_new.png");
 
 export default function App() {
+  const [visibleScreen, setVisibleScreen] = useState("SignIn");
+
+  const toggleVisibleScreen = (screenName) => {
+    setVisibleScreen(screenName);
+  };
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Inter-VariableFont": require("./assets/fonts/Inter-VariableFont.ttf"),
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+
+  if (!fontsLoaded) {
+    return undefined;
+  } else {
+    SplashScreen.hideAsync();
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}
+    >
+      <View style={styles.container}>
+        <ImageBackground source={backgroundImage} style={styles.image}>
+          {visibleScreen === "SignIn" ? (
+            <RegistrationScreen toggleVisibleScreen={toggleVisibleScreen} />
+          ) : (
+            <LoginScreen toggleVisibleScreen={toggleVisibleScreen} />
+          )}
+          {/* <Text style={{ fontFamily: "Inter-VariableFont" }}>HELLO</Text> */}
+        </ImageBackground>
+        <StatusBar style="auto" />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+  },
+  image: {
+    flex: 1,
+    justifyContent: "flex-end",
+    width: "100%",
   },
 });
