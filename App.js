2667ffc5d2e63ddf9a32ca118861064ff4373a40
Nikolay Naumenko <nnv2006@ukr.net>
2023-03-19 23:11:32 +0200

    19-03-2023 v5

--- App.js
+++ App.js
@@ -3,19 +3,12 @@ import { StyleSheet } from "react-native";
 import * as SplashScreen from "expo-splash-screen";
 import { useState, useEffect } from "react";
 import { useFonts } from "expo-font";
-import LoginScreen from "./src/Screens/auth/LoginScreen/LoginScreen";
-import RegistrationScreen from "./src/Screens/auth/RegistrationScreen/RegistrationScreen";
-import Home from "./src/Screens/Home/Home";
-import MapScreen from "./src/Screens/MapScreen/MapScreen";
-import PostsScreen from "./src/Screens/PostsScreen/PostsScreen";
-import ProfileScreen from "./src/Screens/ProfileScreen/ProfileScreen";
-import CreatePostsScreen from "./src/Screens/CreatePostsScreen/CreatePostsScreen";
-import CommentsScreen from "./src/Screens/CommentsScreen/CommentsScreen";
+import { useRoute } from "./router";
+
+// import MapScreen from "./src/Screens/MapScreen/MapScreen";
+// import CommentsScreen from "./src/Screens/CommentsScreen/CommentsScreen";
 
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-const AuthStack = createNativeStackNavigator();
 
 // const backgroundImage = require("./assets/images/bg_new.png");
 
@@ -38,32 +31,7 @@ export default function App() {
     SplashScreen.hideAsync();
   }
 
-  return (
-    <NavigationContainer>
-      <AuthStack.Navigator>
-        <AuthStack.Screen
-          options={{ headerShown: false }}
-          name="Login"
-          component={LoginScreen}
-        />
-        <AuthStack.Screen
-          options={{ headerShown: false }}
-          name="Register"
-          component={RegistrationScreen}
-        />
-      </AuthStack.Navigator>
-    </NavigationContainer>
-  );
-}
+  const routing = useRoute({});
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: "center",
-//   },
-//   image: {
-//     flex: 1,
-//     justifyContent: "flex-end",
-//     width: "100%",
-//   },
-// });
+  return <NavigationContainer>{routing}</NavigationContainer>;
+}
