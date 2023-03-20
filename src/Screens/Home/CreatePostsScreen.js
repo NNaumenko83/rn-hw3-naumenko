1f929288b664ba2d0fc474824666a1ed238f99e2
Nikolay Naumenko <nnv2006@ukr.net>
2023-03-20 20:15:57 +0200

    20-03-2023 v5

--- src/Screens/Home/CreatePostsScreen.js
+++ src/Screens/Home/CreatePostsScreen.js
@@ -8,21 +8,44 @@ import {
   KeyboardAvoidingView,
   ImageBackground,
   Keyboard,
+  Image,
 } from "react-native";
 import { Fontisto } from "@expo/vector-icons";
+import React, { useState, useEffect } from "react";
 
-import { Camera } from "expo-camera";
-
-import { StatusBar } from "expo-status-bar";
-
-const backgroundImage = require("../../../assets/images/bg_new.png");
+import { Camera, CameraType } from "expo-camera";
 
 export default CreatePostsScreen = () => {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [photo, setPhoto] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === "granted");
+    })();
+  }, []);
+
+  const takePhoto = async () => {
+    const photo = await camera.takePictureAsync();
+    setPhoto(photo.uri);
+  };
+
   return (
     <View style={styles.mainContainer}>
-      <Camera style={styles.camera}>
+      <Camera style={styles.camera} ref={(ref) => setCamera(ref)}>
+        {photo && (
+          <View style={styles.takePhotoContainer}>
+            <Image
+              source={{ uri: photo }}
+              style={{ height: 200, width: 200 }}
+            />
+          </View>
+        )}
+
         <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={null}>
+          <TouchableOpacity style={styles.button} onPress={takePhoto}>
             {/* <Text style={styles.text}>Flip Camera</Text> */}
             <Fontisto name="camera" size={20} color="white" />
           </TouchableOpacity>
@@ -53,5 +76,14 @@ const styles = StyleSheet.create({
     marginHorizontal: 16,
     alignItems: "center",
     justifyContent: "center",
+  },
+  takePhotoContainer: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    borderColor: "#ffffff",
+    borderWidth: 1,
+    height: 70,
+    width: 70,
   },
 });
